928c533f71cb0008dae590feafc99335d17b273c
elmasbusenur <elmasbusenur@gmail.com>
2017-06-21 16:10:23 +0300

    global değişknini kullanma

---
+++ GlobalVeYerelDegiskenler.py
@@ -0,0 +1,20 @@
+#def fonksiyon():
+ #   a=5
+  #  print(a)
+#fonksiyon()
+
+#a=10
+#def fonksiyon():
+      #a=5
+      #print(a)
+#fonksiyon()
+#print(a)
+
+a=5
+def fonksiyon():
+    global a
+    a=2
+    print(a)
+fonksiyon()
+print(a)
+
